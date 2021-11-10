d84ea0aba365fe3c0145ff1443437bc7dabd7bd3
binsong.li <binsong.li@tongdun.cn>
2021-11-10 17:43:05 +0800

    reformat code

--- src/main/java/com/github/dzlog/support/ConfigurationLoader.java
+++ src/main/java/com/github/dzlog/support/ConfigurationLoader.java
@@ -25,73 +25,73 @@ import static com.github.dzlog.DzlogConf.DZLOG_DATA_CENTER_KERBEROS_PRINCIPAL;
 @Component
 public class ConfigurationLoader implements InitializingBean {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationLoader.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationLoader.class);
 
-	@Autowired
-	private Environment environment;
+    @Autowired
+    private Environment environment;
 
-	@Autowired
-	private BeeConfigClient configClient;
+    @Autowired
+    private BeeConfigClient configClient;
 
-	private Configuration configuration;
+    private Configuration configuration;
 
-	@Value("${dzlog.datacenter}")
-	private String dataCenter;
+    @Value("${dzlog.datacenter}")
+    private String dataCenter;
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		if (StringUtils.isBlank(dataCenter)) {
-			throw new IllegalArgumentException("dataCenter can not empty");
-		}
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (StringUtils.isBlank(dataCenter)) {
+            throw new IllegalArgumentException("dataCenter can not empty");
+        }
 
-		System.setProperty("HADOOP_USER_NAME", "admin");
-		String configPath = "";
-		try {
-			String profile = "dev";
-			if (ArrayUtils.contains(environment.getActiveProfiles(), "production")) {
-				profile = "production";
-			} else if (ArrayUtils.contains(environment.getActiveProfiles(), "test")) {
-				profile = "test";
-			}
+        System.setProperty("HADOOP_USER_NAME", "admin");
+        String configPath = "";
+        try {
+            String profile = "dev";
+            if (ArrayUtils.contains(environment.getActiveProfiles(), "production")) {
+                profile = "production";
+            } else if (ArrayUtils.contains(environment.getActiveProfiles(), "test")) {
+                profile = "test";
+            }
 
-			LOGGER.info("load hadoop hive hbase config, dataCenter: {}, profile: {}", dataCenter, profile);
-			configPath = "datacenter/" + dataCenter + "/" + profile + "/";
+            LOGGER.info("load hadoop hive hbase config, dataCenter: {}, profile: {}", dataCenter, profile);
+            configPath = "datacenter/" + dataCenter + "/" + profile + "/";
 
-			configuration = new Configuration();
-			ClassPathResource hadoopResource = new ClassPathResource(configPath + "core-site.xml");
-			configuration.addResource(hadoopResource.getInputStream());
-			LOGGER.info("加载文件：{}", hadoopResource.getURL().getPath());
+            configuration = new Configuration();
+            ClassPathResource hadoopResource = new ClassPathResource(configPath + "core-site.xml");
+            configuration.addResource(hadoopResource.getInputStream());
+            LOGGER.info("加载文件：{}", hadoopResource.getURL().getPath());
 
-			String authentication = configuration.get("hadoop.security.authentication");
-			String principal = configClient.getMapString(DZLOG_DATA_CENTER_KERBEROS_PRINCIPAL).get(dataCenter);
+            String authentication = configuration.get("hadoop.security.authentication");
+            String principal = configClient.getMapString(DZLOG_DATA_CENTER_KERBEROS_PRINCIPAL).get(dataCenter);
 
             if ("kerberos".equalsIgnoreCase(authentication)) {
-				Assert.hasText(principal, "启动kerberos，principal 不能为空");
-				LOGGER.info("kerberos 认证");
-				ClassPathResource keytabFile = new ClassPathResource(configPath + "dzlog.keytab");
-				if (!keytabFile.exists()) {
-					throw new IllegalArgumentException("启动kerberos，keytab文件不存在: dzlog.keytab");
-				}
+                Assert.hasText(principal, "启动kerberos，principal 不能为空");
+                LOGGER.info("kerberos 认证");
+                ClassPathResource keytabFile = new ClassPathResource(configPath + "dzlog.keytab");
+                if (!keytabFile.exists()) {
+                    throw new IllegalArgumentException("启动kerberos，keytab文件不存在: dzlog.keytab");
+                }
 
-				ClassPathResource krb5File = new ClassPathResource(configPath + "krb5.conf");
-				if (!krb5File.exists()) {
-					throw new IllegalArgumentException("启动kerberos，krb5.conf 文件不存在: krb5.conf");
-				}
+                ClassPathResource krb5File = new ClassPathResource(configPath + "krb5.conf");
+                if (!krb5File.exists()) {
+                    throw new IllegalArgumentException("启动kerberos，krb5.conf 文件不存在: krb5.conf");
+                }
 
                 try {
-					System.setProperty("java.security.krb5.conf", krb5File.getURL().getPath());
+                    System.setProperty("java.security.krb5.conf", krb5File.getURL().getPath());
                     UserGroupInformation.setConfiguration(configuration);
                     UserGroupInformation.loginUserFromKeytab(principal, keytabFile.getURL().getPath());
                 } catch (IOException e1) {
                     throw new RuntimeException("afterPropertiesSet,kerberos login error: " + e1.getMessage());
                 }
             }
-		} catch (Throwable e) {
-			throw new RuntimeException("加载hadoop、 hive配置文件失败: " + configPath, e);
-		}
-	}
+        } catch (Throwable e) {
+            throw new RuntimeException("加载hadoop、 hive配置文件失败: " + configPath, e);
+        }
+    }
 
-	public Configuration getConfiguration() {
-		return configuration;
-	}
+    public Configuration getConfiguration() {
+        return configuration;
+    }
 }
